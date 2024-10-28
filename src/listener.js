c86bcc4de6e1a940ca801a2a4b01988f1f9f5fe4
FarhanNH <farhan.nhuda@gmail.com>
2024-10-28 10:09:43 +0700

    create openmusic queue consumer apps

---
+++ src/listener.js
@@ -0,0 +1,25 @@
+class Listener {
+  constructor(playlistService, mailSender) {
+    this._playlistService = playlistService;
+    this._mailSender = mailSender;
+
+    this.listen = this.listen.bind(this);
+  }
+
+  async listen(message) {
+    try {
+      const { playlistId, targetEmail } = JSON.parse(message.content.toString());
+
+      const playlist = await this._playlistService.getPlaylists(playlistId);
+
+      const prettyJson = JSON.stringify(playlist, null, 2);
+
+      const result = await this._mailSender.sendEmail(targetEmail, prettyJson);
+      console.log('Email sent result:', result);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+
+module.exports = Listener;
